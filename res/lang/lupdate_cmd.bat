919c1ea51ed3f54344bb900eb835be3e61279943
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2022-08-15 23:29:20 +0900

    さらに修正

--- res/lang/lupdate_cmd.bat
+++ res/lang/lupdate_cmd.bat
@@ -1,4 +1,5 @@
 cd ..
-lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp -ts lang/ja.ts
-lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp -ts lang/en.ts
+cd ..
+lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp -ts res/lang/ja.ts
+lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp -ts res/lang/en.ts
 @PAUSE
