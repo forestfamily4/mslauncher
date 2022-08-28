c1efe6cac06e1773e7c73a9906f4deb65ffad408
forestfamily4 <54976891+forestfamily4@users.noreply.github.com>
2022-08-29 00:40:35 +0900

    lupdate

--- res/lang/en.ts
+++ res/lang/en.ts
@@ -23,7 +23,7 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="42"/>
+        <location filename="../../mainwindow.ui" line="32"/>
         <source>新規サーバー</source>
         <translation>Create New Server</translation>
     </message>
@@ -34,7 +34,7 @@
     </message>
     <message>
         <location filename="../../mainwindow.ui" line="61"/>
-        <location filename="../../mainwindow.cpp" line="193"/>
+        <location filename="../../mainwindow.cpp" line="199"/>
         <source>サーバー起動</source>
         <translation>Launch Server</translation>
     </message>
@@ -54,137 +54,155 @@
         <translation>copy to clipboard</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="107"/>
+        <location filename="../../mainwindow.ui" line="327"/>
+        <source>ディレクトリを開く</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.ui" line="340"/>
+        <source>mslauncher内のJavaを使用する</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;DiscordSRV&lt;/p&gt;&lt;p&gt;Botのtoken&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;DiscordSRV&lt;/p&gt;&lt;p&gt;Bot&apos;s token&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;DiscordSRV&lt;/p&gt;&lt;p&gt;Bot&apos;s token&lt;br/&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="126"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Channelのid&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Channel id&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Channel id&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="158"/>
+        <location filename="../../mainwindow.ui" line="120"/>
         <source>コマンド送信</source>
         <translation>Submit Command</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="165"/>
+        <location filename="../../mainwindow.ui" line="127"/>
         <source>コマンド補完</source>
         <translation>Guess Command</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="199"/>
+        <location filename="../../mainwindow.ui" line="161"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;サーバーのログを表示します。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Display the server logs.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="249"/>
+        <location filename="../../mainwindow.ui" line="211"/>
         <source>コマンドの結果を表示します。</source>
         <translation>Display the command result.</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="278"/>
+        <location filename="../../mainwindow.ui" line="240"/>
         <source>設定</source>
         <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="300"/>
+        <location filename="../../mainwindow.ui" line="262"/>
         <source>サーバーのプロパティ</source>
         <translation>Server Properties</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="323"/>
+        <location filename="../../mainwindow.ui" line="285"/>
         <source>サーバーのディレクトリー</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="35"/>
+        <location filename="../../mainwindow.ui" line="25"/>
         <source>サーバー削除</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="48"/>
+        <location filename="../../mainwindow.cpp" line="50"/>
         <source>サーバーの名前</source>
         <translation>Server name</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="48"/>
+        <location filename="../../mainwindow.cpp" line="50"/>
         <source>新しく作るサーバーの名前を入力してください。</source>
         <translation>Input new server name.</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="50"/>
+        <location filename="../../mainwindow.cpp" line="52"/>
         <source>Open Directory</source>
         <translation>Open Directory</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="74"/>
+        <location filename="../../mainwindow.cpp" line="76"/>
         <source>サーバーの種類を決めてください。</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="97"/>
+        <location filename="../../mainwindow.cpp" line="99"/>
         <source>EULAに同意する必要があります。
 EULA=trueに変更して、もう一度サーバーを起動して下さい。</source>
         <translation>You need to agree with EULA.
 Change it to &quot;EULA=true&quot;, and restart the server.</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="162"/>
+        <location filename="../../mainwindow.cpp" line="166"/>
         <source>サーバーがありません。新規サーバーで新しくサーバーを作ってください。</source>
         <translation>There&apos;s no available servers. Create a new server by clicking &quot;Create New Server&quot; button.</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="170"/>
+        <location filename="../../mainwindow.cpp" line="175"/>
+        <source>Javaがインストールされていない、もしくは環境変数の設定がうまくいってないようです。(環境変数を変えたばかりだと反映されない可能性があります。) mslauncherのフォルダにそのままJREをインストールすることもできますが、どうしますか？</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="186"/>
+        <source>Javaの存在が確かめられませんでした。</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="191"/>
+        <source>javaのバージョンが低いためサーバーが起動できない可能性があります。JRE17以上のバージョンをおすすめします。</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="196"/>
         <source>サーバーストップ</source>
         <translation>Stop the server</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="177"/>
-        <location filename="../../mainwindow.cpp" line="241"/>
-        <location filename="../../mainwindow.cpp" line="398"/>
+        <location filename="../../mainwindow.cpp" line="439"/>
+        <source>対応していません。</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../mainwindow.cpp" line="175"/>
+        <location filename="../../mainwindow.cpp" line="247"/>
+        <location filename="../../mainwindow.cpp" line="400"/>
         <source>確認</source>
         <translation>Confirm</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="177"/>
-        <source>JDKがインストールされていない、もしくは環境変数の設定がうまくいってないようです。(環境変数を変えたばかりだと反映されない可能性があります。) mslauncherのフォルダにそのままJDKをインストールすることもできますが、どうしますか？</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../mainwindow.cpp" line="187"/>
-        <source>JDKの存在が確かめられませんでした。</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../mainwindow.cpp" line="241"/>
+        <location filename="../../mainwindow.cpp" line="247"/>
         <source>すでにサーバーが起動しています。終了しますか？</source>
         <translation>There is already running server. Stop the Server?</translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="328"/>
+        <location filename="../../mainwindow.cpp" line="334"/>
         <source>サーバーが起動していません。</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="331"/>
+        <location filename="../../mainwindow.cpp" line="337"/>
         <source>RCONが有効になっていません。serverのpropertiesの「enable-rcon」をtrueにしてください。</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="334"/>
+        <location filename="../../mainwindow.cpp" line="340"/>
         <source>RCONのパスワードが空白です。serverのpropertiesの「rcon-password」に何らかのパスワードを設定してください。</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="340"/>
+        <location filename="../../mainwindow.cpp" line="346"/>
         <source>サーバーのポートがある整数型にあてはまりません。serverのpropertiesの「rcon-port」に何らかの整数を設定してください。
 初期値の25565をおすすめします。</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.cpp" line="398"/>
+        <location filename="../../mainwindow.cpp" line="400"/>
         <source>サーバーを本当に削除してもいいですか？この操作は取り消せません。</source>
         <translation type="unfinished"></translation>
     </message>
--- res/lang/lupdate_cmd.bat
+++ res/lang/lupdate_cmd.bat
@@ -1,5 +1,5 @@
 cd ..
 cd ..
-lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp back/os.cpp back/java.cpp -ts res/lang/ja.ts
-lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp back/os.cpp back/java.cpp  -ts res/lang/en.ts
+lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp back/os.cpp back/java.cpp servertype/forge.cpp -ts res/lang/ja.ts
+lupdate mainwindow.cpp mainwindow.ui data/data.cpp data/server.cpp data/serverproperty.cpp back/discordsrv.cpp back/commandline.cpp back/rcon.cpp back/downloadmanager.cpp back/os.cpp back/java.cpp  servertype/forge.cpp -ts res/lang/en.ts
 @PAUSE
